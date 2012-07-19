30feecc297968b5f69484a34d92867c444b28155
David Poindexter <davpoind@iupui.edu>
2012-07-19 12:50:09 -0400

    Removed old test code and binding.pry statements.

--- spec/lib/sakai_web_auth_spec.rb
+++ spec/lib/sakai_web_auth_spec.rb
@@ -26,7 +26,7 @@ describe SakaiWeb::Client do
 		end
 	end
 
-	describe "#login" do
+	describe "#login", :focus => true do
 		before(:all) do
 			@config = YAML.load_file(Dir.home + "/.sakai_web_config.yml");
 			@client = SakaiWeb::Client.new
@@ -42,7 +42,7 @@ describe SakaiWeb::Client do
 		it "should need a user and password" do
 			expect { @client.login( @config[:auth_wsdl], {:user => @config[:admin], :pass => @config[:pass]} ) }.to_not raise_error(ArgumentError)
 			@client.user.should eq(@config[:admin])
-			# @client.pass.should eq(config[:pass])
+			@client.pass.should eq(@config[:pass])
 		end
 		it "should not have a session set before authentication" do
 			client = SakaiWeb::Client.new
@@ -52,35 +52,8 @@ describe SakaiWeb::Client do
 			@client.login
 			@client.session.should_not be_nil
 		end
-  #           it "should be able to clusterbust through a load balancer", :focus => true do
-  #             client = SakaiWeb::Client.new
-  #             expect { client.login( "https://stage.oncourse.iu.edu/sakai-axis/SakaiLogin.jws?wsdl" ,
-  #               {:user => "davpoind_admin", :pass => "oncourse is administrated"}) }.to_not raise_error
-  #             expect { client.list_tools_for_site( "SU12-IN-UITS-PRAC-45662", {:service_wsdl => "https://stage.oncourse.iu.edu/sakai-axis/SakaiScript.jws?wsdl"} ) }.to_not raise_error
-  #           end
-		# it "should be able to establish a session, then make repeated calls to the service"  do
-		# 	client = SakaiWeb::Client.new
-		# 	expect { client.login( "https://stage.oncourse.iu.edu/sakai-axis/SakaiLogin.jws?wsdl" ,
-		# 			{:user => "davpoind_admin", :pass => "oncourse is administrated"}) }.to_not raise_error
-		# 	expect {
-		# 			3.times do
-
-		# 				2.times do
-		# 					client.list_tools_for_site( "SU12-IN-UITS-PRAC-45662",
-		# 					{:service_wsdl => "https://stage.oncourse.iu.edu/sakai-axis/SakaiScript.jws?wsdl"} )
-		# 					sleep(2)
-		# 				end
-		# 				sleep(10)
-		# 				3.times do
-		# 					client.list_tools_for_site( "SU12-IN-UITS-PRAC-45662",
-		# 					{:service_wsdl => "https://stage.oncourse.iu.edu/sakai-axis/SakaiScript.jws?wsdl"} )
-		# 					sleep(3)
-		# 				end
-		# 				sleep(10)
-		# 			end
-		# 		}.to_not raise_error
-		# end
 	end
+
 	describe "#loggedin?" do
 		before(:all) do
 			@client = SakaiWeb::Client.new
@@ -93,7 +66,8 @@ describe SakaiWeb::Client do
 			@client.should be_loggedin
 		end
 	end
-	describe "#logout", :focus => true do
+
+	describe "#logout" do
 
 		it "should take a session and logout"
 	end
